5393065bd7368601c91608f0614ca2c9ea57d8ca
AaronARdz <aaron_acostarz@outlook.es>
2021-11-24 00:08:26 -0600

    added bst

---
+++ trees/BinarySearchTree.js
@@ -0,0 +1,49 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while(true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+  lookup(value) {
+
+  }
+}
+
+let newTree = new BinarySearchTree();
+newTree.insert(9)
+newTree.insert(4)
+newTree.insert(6)
+newTree.insert(20)
+newTree.insert(170)
+newTree.insert(15)
+newTree.insert(1)
+console.log(JSON.stringify(newTree))
